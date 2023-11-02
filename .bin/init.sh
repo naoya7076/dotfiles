4249fbafc3d0f4708682745fed2596096c5492e6
Naoya Shimizu <naoya7076@gmail.com>
2023-11-02 23:18:55 +0900

    update init.sh

--- .bin/init.sh
+++ .bin/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
+(
+    echo
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+) >>$HOME/.zprofile
 source $HOME/.zprofile
