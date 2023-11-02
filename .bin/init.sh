0d0d71aaf0433a97074d846b31453efeb026ca18
Naoya Shimizu <naoya7076@gmail.com>
2023-11-02 17:42:25 +0900

    実行権限を追加

--- .bin/init.sh
+++ .bin/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
+source $HOME/.zprofile
