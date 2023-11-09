57f7014a2bbc68bf07afc24d84490426986a0073
Naoya Shimizu <naoya7076@gmail.com>
2023-11-09 11:17:31 +0900

    2本指でクリックを副ボタンのクリックに割り当てる

--- .bin/mac.sh
+++ .bin/mac.sh
@@ -9,3 +9,10 @@ defaults write com.apple.driver.AppleMultitouchTrackpad Clicking -int 1
 
 # dockに最近使ったアプリを表示しない
 defaults write com.apple.dock show-recents -bool false
+
+# 2本指でクリックを副ボタンのクリックに割り当てる
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
+defaults write com.apple.driver.AppleMultitouchTrackpad TrackpadRightClick -int 1
+
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 0
+defaults write com.apple.driver.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 0
