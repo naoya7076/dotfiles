ae7499cecd7ffbc03e5dcb3efb7559de479f6de9
Naoya Shimizu <naoya7076@gmail.com>
2023-11-09 10:41:15 +0900

    defaultsの設定を追加

--- .bin/mac.sh
+++ .bin/mac.sh
@@ -2,3 +2,7 @@
 
 # 隠しファイルを常に表示
 defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# タップしたときに、クリックとする
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write -g com.apple.mouse.tapBehavior -int 1
