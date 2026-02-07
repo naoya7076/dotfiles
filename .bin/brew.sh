2dbab4808920fa467d77e77da98642407952f845
naoya7076 <naoya7076@gmail.com>
2026-02-07 23:18:55 +0900

    update brew.sh

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -euo pipefail
+
+# Brewfileを同期
 chezmoi apply ~/.Brewfile
+
+# インストール・削除・クリーンアップ
 brew bundle --global
+brew bundle cleanup --global --force
+brew autoremove
+brew cleanup
